d067af380aa453021da4485895312e9e1ce9d6b1
Ilya Ternovykh <ily1925@yandex.ru>
2021-11-12 04:26:27 +0300

    feat: сверстать компоненты SearchForm и FilterCheckbox

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,9 +1,11 @@
+import SearchForm from "../SearchForm/SearchForm";
+
 function Movies() {
 
 
   return(
     <main className="movies page-padding">
-      
+      <SearchForm />
     </main>
   );
 }
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,32 @@
+import SearchImage from "../../images/search-form-image.svg"
+import SearchImageButton from "../../images/search-form-button.svg"
+import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
+
+function SearchForm() {
+
+  return (
+    <section className="search-form">
+      <div className="search-form__container">
+        <form className="search-form__form">
+          <div className="search-form__input-group">
+            <img className="search-form__image" src={SearchImage} alt="Изображение поиска" />
+            <input  type="text" id="search-input" name="search-input" placeholder="Фильм" className="search-form__input"  />
+          </div>
+          <div className="search-form__buttons-group">
+            <button type="submit" className="search-form__submit">
+              <img className="search-form__image-button" src={SearchImageButton} alt="Изображение поиска" />
+            </button>
+            <FilterCheckbox 
+              mobileStatus={false}
+            />
+          </div>
+        </form>
+        <FilterCheckbox 
+          mobileStatus={true}
+        />
+      </div>
+    </section>
+  );
+}
+
+export default SearchForm;
